4f2a5781357382205d2b9af372b6e67905ad6ca8
arj0819 <aaronandsydney0819@gmail.com>
2018-09-24 15:25:07 -0500

    Build: Adds support for taking in a file name from the command line, or by using build.xml. Adds some testing code for IOModule constructor

--- project/miniPascalV1/src/Driver.java
+++ project/miniPascalV1/src/Driver.java
@@ -1,9 +1,17 @@
 public class Driver {
 
+    private static String fileName = "";
+
     public static void main(String[] args) {
 
-        System.out.println("IT WORKS YAY");
+        try {
+            fileName = args[0];
+        } catch (Exception e) {
+            System.err.println("Incorrect args provided.\nPlease provide a Pascal file to use with this program.");
+            System.exit(1);
+        }
 
+        IOModule io = new IOModule(fileName);
 
 
 
--- project/miniPascalV1/src/IOModule.java
+++ project/miniPascalV1/src/IOModule.java
@@ -1,3 +1,70 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+
 public class IOModule {
     
+    private String programListing = "";
+
+    public IOModule(String fileName) {
+
+        try {
+            File pascalInput = new File(fileName);
+            BufferedReader rdr = new BufferedReader(new FileReader(pascalInput));
+            String currentLine = "";
+
+            while (rdr.ready()) {
+                currentLine = rdr.readLine();
+                programListing += currentLine+"\n";
+            }
+            programListing = programListing.trim();
+            System.out.println(programListing);
+            // totalSimTime = Integer.parseInt(currentLine);
+
+            // String[] tokens = rdr.readLine().split(" ");
+            // int numServers = Integer.parseInt(tokens[0]);
+            // double probOfTransfer = Double.parseDouble(tokens[3]);
+            // double serviceMean = Double.parseDouble(tokens[2]);
+            // double interArrivalMean = Double.parseDouble(tokens[1]);
+            // CareArea triage = new Triage (numServers,probOfTransfer,serviceMean,interArrivalMean);
+
+            // emergencyDept.put(CareArea.TRIAGE, triage);
+            // transferProbabilities.put(CareArea.TRIAGE, probOfTransfer);
+
+            // tokens = rdr.readLine().split(" ");
+            // numServers = Integer.parseInt(tokens[0]);
+            // probOfTransfer = Double.parseDouble(tokens[2]);
+            // serviceMean = Double.parseDouble(tokens[1]);
+            // CareArea trauma = new Trauma (numServers,probOfTransfer,serviceMean);
+
+            // emergencyDept.put(CareArea.TRAUMA, trauma);
+            // transferProbabilities.put(CareArea.TRAUMA, probOfTransfer);
+
+            // tokens = rdr.readLine().split(" ");
+            // numServers = Integer.parseInt(tokens[0]);
+            // probOfTransfer = Double.parseDouble(tokens[2]);
+            // serviceMean = Double.parseDouble(tokens[1]);
+            // CareArea acute = new Acute (numServers,probOfTransfer,serviceMean);
+
+            // emergencyDept.put(CareArea.ACUTE, acute);
+            // transferProbabilities.put(CareArea.ACUTE, probOfTransfer);
+
+            // tokens = rdr.readLine().split(" ");
+            // numServers = Integer.parseInt(tokens[0]);
+            // probOfTransfer = Double.parseDouble(tokens[2]);
+            // serviceMean = Double.parseDouble(tokens[1]);
+            // CareArea prompt = new Prompt (numServers,probOfTransfer,serviceMean);
+
+            // emergencyDept.put(CareArea.PROMPT, prompt);
+            // transferProbabilities.put(CareArea.PROMPT, probOfTransfer);
+
+            rdr.close();
+
+        } catch (Exception e) {
+            System.err.println("There was a problem reading the file.");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
 }
