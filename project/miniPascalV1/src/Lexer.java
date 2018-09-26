d597ccd4fd83dddaf260d287bd61183ed0b1909c
arj0819 <aaronandsydney0819@gmail.com>
2018-09-26 16:33:06 -0500

    Build: Fixes a bug causing the programCounter to be updated to the lookAheadCounter too early, adds support for many new states and ability to properly analyze them in an input file

--- project/miniPascalV1/src/Lexer.java
+++ project/miniPascalV1/src/Lexer.java
@@ -4,6 +4,7 @@ import java.util.Stack;
 public class Lexer {
 
     private String currentLexeme = "";
+    private String currentChar = "";
     private String detectedLexeme = "";
     private String detectedToken = "";
     private String programText = "";
@@ -28,6 +29,7 @@ public class Lexer {
         // reset the stopSearching flag, currentLexeme, and 
         // detectedLexemeto properly search for the next symbol.
         stopSearching = false;
+        currentChar = "";
         currentLexeme = "";
         detectedLexeme = "";
 
@@ -42,14 +44,6 @@ public class Lexer {
                 break;
             }
 
-            // Set the lookAheadCounter = to the programCounter so we 
-            // know where to start looking ahead from if necessary.
-            lookAheadCounter = programCounter;
-
-            // Set the currentChar to the char in the programText
-            // currently pointed to by the programCounter
-            String currentChar = Character.toString(programText.charAt(programCounter));
-
             // Example of how an exception may be thrown to be handled by the Driver
             // if (programCounter == 150) {
             //     throw new Exception("There was an erroneous token discovered on line "+lineCounter);
@@ -57,75 +51,184 @@ public class Lexer {
 
             // If we found a newline, increment the lineCounter
             if (currentChar.matches(Language.REGEX_NEWLINE)) {
+                System.out.println("Next Line Started");
                 lineCounter++;
             }
 
             System.out.println("CURRENT STATE: "+state);
             switch (state) {
-                case Language.ST_COLON : 
-                    System.out.println("HELLO from Language.ST_COLON");
+                // --------------------\/-------------------- ST_START --------------------\/-------------------- //
+                case Language.ST_START :
+                    // start by increasing the program counter to look at
+                    // the next character in the programText
+                    programCounter++;
+                    // Set the lookAheadCounter = to the programCounter so we 
+                    // know where to start looking ahead from if necessary.
+                    lookAheadCounter = programCounter;
+                    // Set the currentChar to the char in the programText
+                    // currently pointed to by the programCounter
+                    currentChar = Character.toString(programText.charAt(programCounter));
+
+                    // if (currentChar.matches(Language.REGEX_NEWLINE)) {
+                    //     lineCounter++;
+                    // }
+
                     if (currentChar.matches(Language.REGEX_RS_COLON)) {
-                        detectedToken = Language.TOK_RS_COLON;
-                        currentLexeme += currentChar;
-                        detectedLexeme = currentLexeme;
-                        lookAheadCounter++;
-                        currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
-                        state = Language.ST_COLON_EQUALS;
-                        break;
-                    } else {
+                        state = Language.ST_COLON;
+                    } else if (currentChar.matches(Language.REGEX_RS_LCRLYBRACK)) {
                         state = Language.ST_LCRLYBRK;
-                        break;
+                    } else if (currentChar.matches(Language.REGEX_RS_LPAREN)) {
+                        state = Language.ST_LPAREN;
+                    } else if (currentChar.matches(Language.REGEX_RS_RPAREN)) {
+                        state = Language.ST_RPAREN;
                     }
+                    break;
+                // --------------------/\-------------------- ST_START --------------------/\-------------------- //
+                // --------------------\/-------------------- ST_COLON --------------------\/-------------------- //
+                case Language.ST_COLON : 
+                    System.out.println("Entering ST_COLON");
+
+                    detectedToken = Language.TOK_RS_COLON;
+                    currentLexeme += currentChar;
+                    detectedLexeme = currentLexeme;
+                    state = Language.ST_COLON_EQUALS;
+                    break;
+                
+                // --------------------/\-------------------- ST_COLON --------------------/\-------------------- //
+                // --------------------\/-------------------- ST_COLON_EQUALS --------------------\/-------------------- //
                 case Language.ST_COLON_EQUALS :
-                    System.out.println("HELLO from Language.ST_COLON_EQUALS");
+                    System.out.println("Entering ST_COLON_EQUALS");
+                    lookAheadCounter++;
+                    System.out.println("Lookahead counter incremented");
+                    currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
+                    System.out.println(currentLexeme);
                     if (currentLexeme.matches(Language.REGEX_RS_ASSIGN)) {
                         detectedToken = Language.TOK_RS_ASSIGN;
                         detectedLexeme = currentLexeme;
                         programCounter = lookAheadCounter;
 
-                        System.out.println("State Reset by ST_COLON_EQUALS in the if");
+                        System.out.println("State Reset by ST_COLON_EQUALS - found an ASSIGN");
                         resetStateAndStopSearching();
                         break;
                     } else {
-                        System.out.println("State Reset by ST_COLON_EQUALS in the else");
+                        System.out.println("State Reset by ST_COLON_EQUALS - did not find ASSIGN");
                         resetStateAndStopSearching();
                         break;
                     }
+                // --------------------/\-------------------- ST_COLON_EQUALS --------------------/\-------------------- //
+                // --------------------\/-------------------- ST_LCRLYBRK --------------------\/-------------------- //
                 case Language.ST_LCRLYBRK :
-                    System.out.println("HELLO from Language.ST_LCRLYBRK");
-                    if (currentChar.matches(Language.REGEX_RS_LCRLYBRACK)) {
-                        detectedToken = Language.TOK_LP_COMMENT;
-                        currentLexeme += currentChar;
-                        detectedLexeme = currentLexeme;
-                        lookAheadCounter++;
-                        currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
-                        state = Language.ST_LCRLYBRK_IGNOREALL;
-                        break;
+                    System.out.println("Entering ST_LCRLYBRK");
+                    detectedToken = Language.TOK_LP_COMMENT;
+                    currentLexeme += currentChar;
+                    detectedLexeme = currentLexeme;
+                    lookAheadCounter++;
+                    if (Character.toString(programText.charAt(lookAheadCounter)).equals(Language.REGEX_NEWLINE)) {
+                        System.out.println("Next Line Started");
+                        lineCounter++;
+                        currentLexeme += Character.toString(programText.charAt(lookAheadCounter)).replace("\n","\\n");
                     } else {
-                        System.out.println("State Reset by ST_LCRLYBRK in the else");
-                        resetStateAndStopSearching();
-                        break;
+                        currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
                     }
+                    System.out.println("Leaving state ST_LCRLYBRK - found a LCRLYBRACK");
+                    state = Language.ST_LCRLYBRK_IGNOREALL;
+                    break;
+                // --------------------/\-------------------- ST_LCRLYBRK --------------------/\-------------------- //
+                // --------------------\/-------------------- ST_LCRLYBRK_IGNOREALL --------------------\/-------------------- //
                 case Language.ST_LCRLYBRK_IGNOREALL :
-                    System.out.println("HELLO from Language.ST_LCRLYBRK_IGNOREALL");
-                    if (currentLexeme.matches(Language.REGEX_PT_COMMENT)) {
+                    System.out.println("Entering ST_LCRLYBRK_IGNOREALL");
+                    if (currentLexeme.matches(Language.REGEX_PT_CRLYCOMMENT)) {
                         detectedLexeme = currentLexeme;
                         programCounter = lookAheadCounter;
 
-                        System.out.println("State Reset by ST_LCRLYBRK_IGNOREALL in the if");
+                        System.out.println("State Reset by ST_LCRLYBRK_IGNOREALL - found a COMMENT");
                         resetStateAndStopSearching();
                         break;
                     } else {
                         lookAheadCounter++;
-                        currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
+                        System.out.println("Lookahead counter incremented");
+                        System.out.println("No state change - have not yet detected RCRLYBRACK");
+                        if (Character.toString(programText.charAt(lookAheadCounter)).equals(Language.REGEX_NEWLINE)) {
+                            System.out.println("Next Line Started");
+                            lineCounter++;
+                            currentLexeme += Character.toString(programText.charAt(lookAheadCounter)).replace("\n","\\n");
+                        } else {
+                            currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
+                        }
                         break;
                     }
+                // --------------------/\-------------------- ST_LCRLYBRK_IGNOREALL --------------------/\-------------------- //
+                // --------------------\/-------------------- ST_LPAREN --------------------\/-------------------- //
+                case Language.ST_LPAREN :
+                    System.out.println("Entering ST_LPAREN");
+                    detectedToken = Language.TOK_RS_LPAREN;
+                    currentLexeme += currentChar;
+                    detectedLexeme = currentLexeme;
+                    state = Language.ST_LBIGRAM;
+                    break;
+                // --------------------/\-------------------- ST_LPAREN --------------------/\-------------------- //
+                // --------------------\/-------------------- ST_LBIGRAM --------------------\/-------------------- //
+                case Language.ST_LBIGRAM :
+                    System.out.println("Entering ST_LBIGRAM");
+                    System.out.println(currentLexeme);
+                    lookAheadCounter++;
+                    System.out.println("Lookahead counter incremented");
+                    currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
+                    if (currentLexeme.matches(Language.REGEX_RS_LBIGRAM)) {
+                        detectedToken = Language.TOK_LP_COMMENT;
+                        detectedLexeme = currentLexeme;
+                        lookAheadCounter++;
+                        currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
+                        System.out.println("Leaving state ST_BIGRAM - found a LBIGRAM");
+                        state = Language.ST_LBIGRAM_IGNOREALL;
+                        break;
+                    } else {
+                        System.out.println("State Reset by ST_LBIGRAM - did not find LBIGRAM");
+                        resetStateAndStopSearching();
+                        break;
+                    }
+                // --------------------/\-------------------- ST_LBIGRAM --------------------/\-------------------- //
+                // --------------------\/-------------------- ST_LBIGRAM_IGNOREALL --------------------\/-------------------- //
+                case Language.ST_LBIGRAM_IGNOREALL :
+                    System.out.println("Entering ST_LBIGRAM_IGNOREALL");
+                    if (currentLexeme.matches(Language.REGEX_PT_BGRMCOMMENT)) {
+                        detectedLexeme = currentLexeme;
+                        programCounter = lookAheadCounter;
+
+                        System.out.println("State Reset by ST_LBIGRAM_IGNOREALL - found a COMMENT");
+                        resetStateAndStopSearching();
+                        break;
+                    } else {
+                        lookAheadCounter++;
+                        System.out.println("Lookahead counter incremented");
+                        System.out.println("No state change - have not yet detected RBIGRAM");
+                        if (Character.toString(programText.charAt(lookAheadCounter)).equals(Language.REGEX_NEWLINE)) {
+                            System.out.println("Next Line Started");
+                            lineCounter++;
+                            currentLexeme += Character.toString(programText.charAt(lookAheadCounter)).replace("\n","\\n");
+                        } else {
+                            currentLexeme += Character.toString(programText.charAt(lookAheadCounter));
+                        }
+                        break;
+                    }
+                // --------------------/\-------------------- ST_LBIGRAM_IGNOREALL --------------------/\-------------------- //
+                // --------------------\/-------------------- ST_RPAREN --------------------\/-------------------- //
+                case Language.ST_RPAREN :
+                    System.out.println("Entering ST_RPAREN");
+                    detectedToken = Language.TOK_RS_RPAREN;
+                    currentLexeme += currentChar;
+                    detectedLexeme = currentLexeme;
+                    System.out.println("State Reset by ST_RPAREN - found a RPAREN");
+                    resetStateAndStopSearching();
+                break;
+            // --------------------/\-------------------- ST_RPAREN --------------------/\-------------------- //
+                // --------------------\/-------------------- DEFAULT --------------------\/-------------------- //
                 default:
-                    System.out.println("State Reset by DEFAULT CASE");
+                    System.out.println("State Reset by default case");
                     resetStateAndStopSearching();
                     break;
+                // --------------------/\-------------------- DEFAULT --------------------/\-------------------- //
             }
-            programCounter++;
         }        
 
         /*
@@ -157,12 +260,12 @@ public class Lexer {
         return detectedLexeme;
     }
     public void resetStateAndStopSearching() {
-        state=0;
+        state = Language.ST_START;
         stopSearching();
     }
     public void resetState() {
         System.out.println("STATE RESET from resetState()");
-        state=0;
+        state = Language.ST_START;
     }
     public void stopSearching() {
         stopSearching = true;
