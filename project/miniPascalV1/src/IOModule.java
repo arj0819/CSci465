b626e41d8fa107a144822cec98c01aca5c273d39
arj0819 <aaronandsydney0819@gmail.com>
2018-09-26 22:51:46 -0500

    Build: Adds descriptive comments

--- project/miniPascalV1/src/IOModule.java
+++ project/miniPascalV1/src/IOModule.java
@@ -4,13 +4,28 @@ import java.io.File;
 import java.util.List;
 import java.util.ArrayList;
 
+/*
+    IOModule uses the following importd:
+    BufferedReader - used to read the input file
+    FileReader - used to construct an object readable by BufferedReaader
+    File - used to construct an object for the actual input file
+    List - used to generically loop through Matches ArrayList
+    ArrayList - used to create an ArrayList of Matches
+*/
+
 public class IOModule {
     
+    // The IOModule has programText, which is built as
+    // the input file is read in, and matches, which is
+    // a list to contain all symbol matches found by the
+    // Lexer.
+
     private String programText = "";
     private List<Match> matches = new ArrayList<Match>();
 
     public IOModule(String fileName) {
 
+        // Try to construct and read from the input file.
         try {
             File pascalInput = new File(fileName);
             BufferedReader rdr = new BufferedReader(new FileReader(pascalInput));
@@ -22,29 +37,34 @@ public class IOModule {
             }
             programText = programText.trim();
             rdr.close();
-            System.out.println(programText);
+            // System.out.println(programText);
         } catch (Exception e) {
             System.err.println("There was a problem reading the file.");
             System.err.println(e.getMessage());
             System.exit(1);
         }
+
+        // These are adding dummy Matches to the matches 
+        // list to provide better output formatting, that all!
         this.addMatch(new Match("TOKEN","LEXEME"));
         this.addMatch(new Match("",""));
     }
 
+    // Returns the programText read from the input file
     public String getProgramText() {
         return programText;
     }
 
+    // Adds a symbol match to the matches list
     public void addMatch(Match newMatch) {
         matches.add(newMatch);
     }
 
+    // Prints out all matches in the matches list
     public void printMatches() {
         System.out.println();
         for (Match m : matches) {
             System.out.println(m);
         }
     }
-
 }
