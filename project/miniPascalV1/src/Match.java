20829961910b7c46bc962b9e32f04bc066f03ed6
arj0819 <aaronandsydney0819@gmail.com>
2018-09-26 22:19:19 -0500

    Build: Adds descriptive comments

--- project/miniPascalV1/src/Match.java
+++ project/miniPascalV1/src/Match.java
@@ -1,16 +1,26 @@
 public class Match {
 
+    // A Match consists of a token and a lexeme,
+    // along with a matchID in case we need to 
+    // identify matches later in the project.
+    // The Match Class has a static int field
+    // to keep track of the totalMatches created.
     private static int totalMatches = 0;
     private int matchID = 0;
     private String lexeme = "";
     private String token = "";
 
+    // Constructor assigns the token and lexeme,
+    // and gives this instance a matchID while
+    // incrementing the totalMatches.
     public Match (String token, String lexeme) {
         this.lexeme = lexeme;
         this.token = token;
         this.matchID = ++totalMatches;
     }
 
+    // Match overrides toString for 
+    // convenient printout.
     @Override
     public String toString() {
         String str = "%-20s %-20s";
@@ -21,5 +31,4 @@ public class Match {
         );
         return str;
     }
-
 }
