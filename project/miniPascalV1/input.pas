af1d2a82fc904ab32f1e5259d3080dd04d47fda4
arj0819 <aaronandsydney0819@gmail.com>
2018-09-26 20:30:45 -0500

    Build: Adds more test cases for newly supported symbols

--- project/miniPascalV1/input.pas
+++ project/miniPascalV1/input.pas
@@ -2,7 +2,12 @@ program example(input,output);
     var x,y:integer;
     function gcd(a,b:integer):integer;
     begin{gcd}
-        if b=0then gcd:=a else gcd:=(b,a mod b)
+        if b=0then gcd:=a else gcd:=(b,a mod b) {Option1}
+        if b<0then gcd:=c else gcd:=(b,c mod b) {Option2}
+        if b<=0then gcd:=d else gcd:=(b,d mod b) (*Option3*)
+        if b<>0then gcd:=e else gcd:=(b,e mod b) (*Option4*)
+        if b>0then gcd:=f else gcd:=(b,f mod b) {Option5}
+        if b>=0then gcd:=g else gcd:=(b,g mod b) {Option6}
     end;{gcd}
     begin{example}
         read(x,y);
