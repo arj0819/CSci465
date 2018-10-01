76af519817084dea28644c10986b54da6080bd28
arj0819 <aaronandsydney0819@gmail.com>
2018-09-30 21:41:45 -0500

    Build: Adds new testing cases for while loop and add operator

--- project/miniPascalV1/input.pas
+++ project/miniPascalV1/input.pas
@@ -1,13 +1,14 @@
 program example(input,output);
     var x,y:integer;
+    n: array [1..20] of integer;
+    chararray: array[char] of 1..26;
+    arrayOfChars: array[char] of 1..5;
     function gcd(a,b:integer):integer;
     begin{gcd}
-        if b=0 then gcd:=a else gcd:=(b,a mod b) {Option1}
-        if b<1 then gcd:=c else gcd:=(b,c mod b) {Option2}
-        if b<=2 then gcd:=d else gcd:=(b,d mod b) (*Option3*)
-        if b<>3.4 then gcd:=e else gcd:=(b,e mod b) (*Option4*)
-        if b>5.67 then gcd:=f else gcd:=(b,f mod b) {Option5}
-        if b>=8.912 then gcd:=g else gcd:=(b,g mod b) {Option6}
+        while b<10 do
+        begin
+            a := a + 1;
+        end;
     end;{gcd}
     begin{example}
         read(x,y);
