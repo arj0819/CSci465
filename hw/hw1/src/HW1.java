5dce2f921fdfc16ede213953fd22cf501602a806
arj0819 <aaronandsydney0819@gmail.com>
2018-10-10 16:59:20 -0500

    Build: working on finishing hw1

--- hw/hw1/src/HW1.java
+++ hw/hw1/src/HW1.java
@@ -3,12 +3,28 @@ import java.io.FileReader;
 import java.io.File;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.regex.*;
+import java.util.Stack;
 
+/*
+    Author: Aaron Johnson
+    This program will translate arithmetic
+    expressions in input.txt from infix
+    notation to postfix notation.
+*/
 
 public class HW1 {
 
+    public static final String MULT = "\\*";
+    public static final String DIV  = "div";
+    public static final String MOD  = "mod";
+    public static final String PLUS = "\\+";
+    public static final String MINUS = "-";
+
+    public static Stack<String> operatorStack = new Stack<String>();
     public static void main(String[] args) {
 
+        
         // Try to construct and read from the input file.
         try {
             String fileName = args[0];
@@ -19,14 +35,51 @@ public class HW1 {
             while (rdr.ready()) {
                 currentLine = rdr.readLine();
                 String[] tokens = currentLine.split(" ");
-                // System.out.println(currentLine);
+                System.out.println("Infix:\n"+currentLine+"\nPostfix:");
 
                 for (int i = 0; i < tokens.length; i++) {
-                    System.out.println(tokens[i]);
+                    // System.out.println(tokens[i]);
+
+                    if(tokens[i].matches("\\d+")) {
+                        System.out.print(tokens[i]+" ");
+                    }
+                    if (operatorStack.isEmpty()) {
+                        if (tokens[i].matches(MULT)) {
+                            operatorStack.push(tokens[i]);
+                        } else if (tokens[i].matches(DIV)) {
+                            operatorStack.push(tokens[i]);
+                        } else if (tokens[i].matches(MOD)) {
+                            operatorStack.push(tokens[i]);
+                        } else if (tokens[i].matches(PLUS)) {
+                            operatorStack.push(tokens[i]);
+                        } else if (tokens[i].matches(MINUS)) {
+                            operatorStack.push(tokens[i]);
+                        } else {
+                            // System.out.print("Unrecognized operator");
+                        }
+                    } else if (operatorStack.peek().matches(PLUS) || operatorStack.peek().matches(MINUS)) {
+                        if (tokens[i].matches(MULT)) {
+                            operatorStack.push(tokens[i]);
+                        } else if (tokens[i].matches(DIV)) {
+                            operatorStack.push(tokens[i]);
+                        } else if (tokens[i].matches(MOD)) {
+                            operatorStack.push(tokens[i]);
+                        } else if (tokens[i].matches(PLUS)) {
+                            operatorStack.push(tokens[i]);
+                        } else if (tokens[i].matches(MINUS)) {
+                            operatorStack.push(tokens[i]);
+                        } else {
+                            // System.out.print("Unrecognized operator");
+                        }
+                    } else {
+                        System.out.print(operatorStack.pop()+" ");
+                    }
+                    
+
                 }
+                System.out.println("\n");
             }
             rdr.close();
-            // System.out.println(programText);
         } catch (Exception e) {
             System.err.println("There was a problem reading the file.");
             System.err.println(e.getMessage());
